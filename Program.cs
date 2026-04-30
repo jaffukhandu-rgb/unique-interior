8ad88bead5d4c416998bf4eb4ab1efe3218ca9d1
Jafar <jaffukhandu@gmail.com>
2026-05-01 02:56:48 +0530

    fix error page

--- Program.cs
+++ Program.cs
@@ -3,41 +3,38 @@ using Unique_1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddDbContext<InteriorShopDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddControllersWithViews();
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
 
 builder.Services.AddSession();
 
-
 var app = builder.Build();
-app.UseDeveloperExceptionPage();
 
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// ✅ THIS IS CORRECT WAY
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // SHOW REAL ERROR
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
-app.UseSession();
+app.UseStaticFiles();
 
+app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
-app.UseStaticFiles();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}")
-    ;
-
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
